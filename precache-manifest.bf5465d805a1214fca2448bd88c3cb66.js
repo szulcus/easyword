59f8ff52616d7ed2dc33f4f4061b5c244650b5b9
Jakub Schulz <jakub104@Dell-Komputer.localdomain>
2019-05-15 22:00:58 +0200

    Updates

---
+++ precache-manifest.bf5465d805a1214fca2448bd88c3cb66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438b6b0bc758578e291b2f1e39215153",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "91720e03f11faddaa146",
+    "url": "/deploy-react/static/css/main.4488b0a6.chunk.css"
+  },
+  {
+    "revision": "0d1414ad7408a72da6e2",
+    "url": "/deploy-react/static/js/2.f16a29b9.chunk.js"
+  },
+  {
+    "revision": "91720e03f11faddaa146",
+    "url": "/deploy-react/static/js/main.3371e47b.chunk.js"
+  },
+  {
+    "revision": "a70ab81c67defc9fa7dc",
+    "url": "/deploy-react/static/js/runtime~main.5de6419e.js"
+  },
+  {
+    "revision": "35cbfcc38a9b754dea1170c88fbffeea",
+    "url": "/deploy-react/static/media/information.35cbfcc3.png"
+  }
+]);
