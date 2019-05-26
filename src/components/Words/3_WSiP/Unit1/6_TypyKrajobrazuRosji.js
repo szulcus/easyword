aac9f3d09ea36c2eeb4702765cedc940d2ce4a10
Jakub Schulz <jakub104@Dell-Komputer.localdomain>
2019-05-26 18:23:52 +0200

    typy krajobrazu rozji added

--- src/components/Words/3_WSiP/Unit1/6_TypyKrajobrazuRosji.js
+++ src/components/Words/3_WSiP/Unit1/6_TypyKrajobrazuRosji.js
@@ -3,32 +3,32 @@ const words6_TypyKrajobrazuRosji= [
         word: `пусты́ня`,
         translation: `pustynia`,
         type: `typy krajobrazu rosji`,
-        image: `url`,
+        image: `https://c.pxhere.com/images/f0/5d/73a6752205b1e6db896bd9424161-1427841.jpg!s`,
     },
     {
         word: `степь`,
         translation: `step`,
         type: `typy krajobrazu rosji`,
-        image: `url`,
+        image: `https://c.pxhere.com/photos/5e/4d/prairie_steppes_mountains_vegetation_nature_birds_fly_beauty-677703.jpg!s`,
     },
     {
         word: `субтро́пики`,
         translation: `subtropiki`,
         translation2: `strefa podzwrotnikowa`,
         type: `typy krajobrazu rosji`,
-        image: `url`,
+        image: `https://c.pxhere.com/images/b7/98/f69936e252ab1143edd41446563f-1417284.jpg!s`,
     },
     {
         word: `тайга́`,
         translation: `tajga`,
         type: `typy krajobrazu rosji`,
-        image: `url`,
+        image: `https://c.pxhere.com/photos/37/c0/finland_forest_taiga_lapland_lichen-1265320.jpg!s`,
     },
     {
         word: `ту́ндра`,
         translation: `tundra`,
         type: `typy krajobrazu rosji`,
-        image: `url`,
+        image: `https://c.pxhere.com/images/cc/6a/aa9d3d56d90df8a7ec605cd903c9-1438609.jpg!s`,
     },
 ]
 
