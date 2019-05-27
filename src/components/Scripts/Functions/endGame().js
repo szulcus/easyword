bc95f4ea69f7d004ced5bd2afb6a568a96f4afa7
Jakub Schulz <jakub104@Dell-Komputer.localdomain>
2019-05-27 21:45:43 +0200

    last presentation update

--- src/components/Scripts/Functions/endGame().js
+++ src/components/Scripts/Functions/endGame().js
@@ -3,7 +3,11 @@ function endGame() {
 	const socialMedia = document.getElementById('social-media');
 	const background = document.getElementById('end-game');
 	const information = document.getElementById('information');
+	const userText = document.getElementById('userText');
+	const answer = document.getElementById('answer');
 
+	answer.style.display = 'none';
+	userText.style.display = 'none';
 	background.style.display = 'flex';
 	information.style.display = 'block';
 	socialMedia.style.transform = 'translateY(-10vh)';
